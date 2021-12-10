393f21a1dd7b7bda48399abc9a883ef220596834
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-12-10 09:53:18 +0900

    기능 개발

--- 13.기능개발/13.기능개발.js
+++ 13.기능개발/13.기능개발.js
@@ -1,23 +1,30 @@
-const progresses = [93, 30, 55];
-const speeds = [1, 30, 5];
+const progresses = [95, 90, 99, 99, 80, 99];
+const speeds = [1, 1, 1, 1, 1, 1];
 
 function solution(progresses, speeds) {
   let day = 0;
   let count = 0;
   const answer = [];
-  while (progresses.length !== 0) {
-    if (progresses[0] + day * speeds[0] >= 100) {
-      console.log("여기?");
-      progresses.slice();
-      speeds.slice();
+  let progress = 0;
+  while (progresses[0]) {
+    progress = progresses[0] + day * speeds[0];
+    if (progress >= 100) {
+      // 배포 가능하면
       count++;
-      continue;
+      progresses.shift();
+      speeds.shift();
     } else {
-      answer.push(count);
+      // 배포 불가능하면 뭘 해야 하지?
+      // 배포 완료된 기능이 있는 경우
+      if (count) {
+        answer.push(count);
+      }
+      day++;
       count = 0;
     }
-    day++;
   }
+  answer.push(count);
+
   return answer;
 }
 
